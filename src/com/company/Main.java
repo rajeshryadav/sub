4d7d3b3b2a35cc343368758c36db792acbc123d6
Rahul kumar <rahulkumar96369@gmail.com>
2019-09-12 15:22:15 +0530

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 5;
+        int c = a-b;
+        System.out.println(c);
+    }
+}
